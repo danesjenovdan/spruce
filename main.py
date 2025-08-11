e0aa3156c0fbe87fe2062737c3bd8b43a5e42995
LepkoQQ <lepko.san@gmail.com>
2025-08-11 14:25:25 +0200

    cleanup and fix import

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from os.path import dirname, join
 from dotenv import load_dotenv
 
 from spruce.github_auth import github_login
-from spruce.outdated import main as outdated, main_all as outdated_all
+from spruce.outdated import main as outdated
+from spruce.outdated import main_all as outdated_all
 from spruce.repositories import check_repositories
 
 dotenv_path = join(dirname(__file__), ".env")
